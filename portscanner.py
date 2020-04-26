1cd24b3c8041f343c523459f1eec6e51b8f5a36a
martiph <philip_marti@besonet.ch>
2020-04-26 08:19:55 +0200

    Added ability to provide the parameters for the script on the command line. Added some help too.

--- portscanner.py
+++ portscanner.py
@@ -5,14 +5,24 @@ from datetime import datetime
 # copied some things from here: https://www.pythonforbeginners.com/code-snippets-source-code/port-scanner-in-python/
 
 # Ask for server and ports to scan
-remote_server = input("Enter a remote host to scan: ")
-port_number = input("Enter the highest port number you want to scan: ")
+if len(sys.argv) != 2:
+    print("-"*80)
+    print("Usage of Fingerprinter.portscanner.py:\n./portscanner.py <target-ip-address> <highest_port_in_range>\n")
+    print("Example:\n./portscanner.py 127.0.0.1 1000\nThis will scan \'localhost\' in portrange 1 to 1000 (reserved "
+          "ports).")
+    print("-"*80)
+    sys.exit()
+else:
+    remote_server = sys.argv[0]
+    port_number = sys.argv[1]
+    # TODO: Do some input validation on the ip address and the port number
+
 remote_server_ip = socket.gethostbyname(remote_server)
 
 # Print a banner with information about the host
-print("-" * 60)
+print("-" * 80)
 print("Please wait, scanning remote host {0} in port range 1 to {1}".format(remote_server_ip, port_number))
-print("-" * 60)
+print("-" * 80)
 
 # Check what time the scan started
 t1 = datetime.now()
