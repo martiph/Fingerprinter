d9b551d3081cacf5ba9aea27e5a3d9a281d8447d
martiph <philip_marti@besonet.ch>
2020-05-10 22:45:45 +0200

    moved related files to "PacketCrafter", fixed error in handling the carry-bit during ones complement addition

--- PacketCrafter/calculate_header_checksum.py
+++ PacketCrafter/calculate_header_checksum.py
@@ -22,37 +22,38 @@ def ip(packet=""):
     # Example Internet Datagram Header
     # To compute the checksum, the checksum-field is set to 0.
     if packet != "":
-        word = packet.split(" ")
-        print(word)
-        for i in range(len(word)):
-            print(word[i])
-            if len(word[i]) != 4:
+        ip_header = packet.split(" ")
+        for i in range(len(ip_header)):
+            print(ip_header[i])
+            if len(ip_header[i]) != 4:
                 try:
-                    hex(int(word[i], 16))
+                    hex(int(ip_header[i], 16))
                 except TypeError:
                     print("TypeError occurred.")
                 finally:
-                    print("Word " + word[i] + " does not meet the expected format. You must provide a string with "
-                                              "multiple four HEX-value words separated by a whitespace.")
+                    print("Word " + ip_header[i] + " does not meet the expected format. You must provide a string with "
+                                                   "multiple four HEX-value words separated by a whitespace.")
                     return None
-            word[i] = int(word[i], 16)
+            ip_header[i] = int(ip_header[i], 16)
+            print(ip_header[i])
     else:
-        print("Please provide the 16-bit words in HEX-format (without preceding 0x). Example for a 16-bit word: abcd")
+        print(
+            "Please provide the 16-bit words in HEX-format (without preceding 0x). Example for a 16-bit ip_header: abcd")
         print("It is assumed that the IHL is 5.")
 
-        word = []
-        word.append(int(input("Version, IHL and ToS: "), 16))
-        word.append(int(input("Total Length: "), 16))
-        word.append(int(input("Identification: "), 16))
-        word.append(int(input("Flags and Fragment Offset: "), 16))
-        word.append(int(input("TTL and Protocol: "), 16))
-        word.append(int("0", 16))  # checksum-field is during the computation set to 0
-        word.append(int(input("First Part of Source IP-Address: "), 16))
-        word.append(int(input("Second Part of Source IP-Address: "), 16))
-        word.append(int(input("First Part of Destination IP-Address: "), 16))
-        word.append(int(input("Second Part of Destination IP-Address: "), 16))
+        ip_header = []
+        ip_header.append(int(input("Version, IHL and ToS: "), 16))
+        ip_header.append(int(input("Total Length: "), 16))
+        ip_header.append(int(input("Identification: "), 16))
+        ip_header.append(int(input("Flags and Fragment Offset: "), 16))
+        ip_header.append(int(input("TTL and Protocol: "), 16))
+        ip_header.append(int("0", 16))  # checksum-field is during the computation set to 0
+        ip_header.append(int(input("First Part of Source IP-Address: "), 16))
+        ip_header.append(int(input("Second Part of Source IP-Address: "), 16))
+        ip_header.append(int(input("First Part of Destination IP-Address: "), 16))
+        ip_header.append(int(input("Second Part of Destination IP-Address: "), 16))
 
-    checksum = calc_checksum(word)
+    checksum = calc_checksum(ip_header)
     print("Checksum as Integer (DEC): " + str(checksum))
     return hex(checksum)
 
@@ -87,6 +88,9 @@ def tcp(src_ip, dest_ip, protocol="06", tcp_segment=""):
     for i in range(len(tcp_segment)):  # do some input validation
         try:
             if len(tcp_segment[i]) != 4:
+                if i == (len(tcp_segment) - 1):
+                    while len(tcp_segment[i]) < 4:
+                        tcp_segment[i] += '0'
                 raise ValueError
             hex(int(tcp_segment[i], 16))
         except TypeError:
@@ -95,13 +99,10 @@ def tcp(src_ip, dest_ip, protocol="06", tcp_segment=""):
         except ValueError:
             print("Please provide the input in form of 16-bit HEX-words separated by a whitespace.")
             return None
-    segment_length = 0  # 16 bit long, shows how long the whole segment in bytes is
-    segment_list = tcp_segment.list(" ")
-    for word in segment_list:
-        segment_length += 2
+    segment_length = 2 * len(tcp_segment)  # 16 bit long, shows how long the whole segment in bytes is
     segment_length = hex(segment_length)[2:]
     pseudo_header = src_ip + " " + dest_ip + " 00" + protocol + " " + segment_length  # 96 bit long (3 x 32 bit)
-    pseudo_complete_segment = pseudo_header + tcp_segment
+    pseudo_complete_segment = pseudo_header + ' '.join(tcp_segment)
     if len(pseudo_complete_segment) % 2 == 1:
         pseudo_complete_segment += " 0000"
     pseudo_complete_segment = pseudo_complete_segment.split(" ")
@@ -119,16 +120,17 @@ def ones_complement_addition(number1, number2):
     if not (isinstance(number1, int)) and not (isinstance(number2, int)):
         return None
     result = bin(number1 + number2)  # string will begin with '0b', just ignore result[0] and result[1]
-    if result[2] == '1':
-        if len(result) < 18:  # add leading zeros
-            partial_result = result[2:]
-            while len(partial_result) < 16:
-                partial_result = '0' + partial_result
-            result = '0b' + partial_result
+
+    if len(result) < 18:  # add leading zeros
+        partial_result = result[2:]
+        while len(partial_result) < 16:
+            partial_result = '0' + partial_result
+        result = '0b' + partial_result
+
     if len(result) > 18:
-        if len(result) == 19 and result[2] == '1':
+        if len(result) == 19 and result[2] == '1':  # TODO: Add code to handle the cases where the result is even longer
             print("carry bit needed")
-            carry_bit = result[2]
+            carry_bit = '1'
             result = list(result)  # convert the string to a list
             result.pop(2)
             print(result)
@@ -136,7 +138,7 @@ def ones_complement_addition(number1, number2):
                 if result[-i] == '0' and carry_bit == '1':
                     result[-i] = '1'
                     carry_bit = '0'
-                elif result[-i] == '1':
+                elif result[-i] == '1' and carry_bit == '1':
                     result[-i] = '0'
                     carry_bit = '1'
                 elif carry_bit == '0':
@@ -145,6 +147,7 @@ def ones_complement_addition(number1, number2):
                     # this should never be executed
                     carry_bit = '0'
             result = ''.join(result)  # convert the list to a string
+    print("Result of ones complement addition: " + result)
     return int(result, 2)
 
 
@@ -199,4 +202,9 @@ def main(protocol):
         print("-" * 80)
         sys.exit(1)
 
+
 # main(input("IP or TCP checksum? "))
+
+# ip("4500 00cd 29e7 4000 8006 0000 c0a8 009e c0a8 000c")
+
+tcp("c0a8 009e", "c0a8 000c", "06", "060c cc63 5f8d 1e40 6923 1857 5018 faf0 0000 0000 4745 5420 2f64 6d72 2e78 6d6c 2048 5454 502f 312e 310d 0a55 7365 722d 4167 656e 743a 2053 706f 7469 6679 2f31 3133 3030 3036 3538 2057 696e 3332 2f30 2028 5043 2064 6573 6b74 6f70 290d 0a48 6f73 743a 2031 3932 2e31 3638 2e30 2e31 323a 3532 3332 330d 0a4b 6565 702d 416c 6976 653a 2030 0d0a 4163 6365 7074 2d45 6e63 6f64 696e 673a 2067 7a69 700d 0a43 6f6e 6e65 6374 696f 6e3a 206b 6565 702d 616c 6976 650d 0a0d 0a00")
