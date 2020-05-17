4a7d345094cab2e6e1e1f97a8130341761dd8866
martiph <philip_marti@besonet.ch>
2020-05-18 00:06:20 +0200

    Cleaned up the ip method. Completely refactored the tcp method. Added more comments/docstrings. Improved calculation of TCP-Segment length.

--- PacketCrafter/calculate_header_checksum.py
+++ PacketCrafter/calculate_header_checksum.py
@@ -1,7 +1,14 @@
 import sys
 
 
-def ip(packet=""):
+def ip(packet):
+    """
+    Used to calculate the checksum of an IP-Packet
+
+
+    :param packet: The IP-packet header, in form of 16-bit words in HEX (without preceding 0x)
+    :return:
+    """
     # source: RFC 791; https://tools.ietf.org/html/rfc791#section-3.1
     # 0                   1                   2                   3
     # 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
@@ -21,24 +28,9 @@ def ip(packet=""):
     #
     # Example Internet Datagram Header
     # To compute the checksum, the checksum-field is set to 0.
-    if packet != "":
-        ip_header = packet.split(" ")
-        for i in range(len(ip_header)):
-            print(ip_header[i])
-            if len(ip_header[i]) != 4:
-                try:
-                    hex(int(ip_header[i], 16))
-                except TypeError:
-                    print("TypeError occurred.")
-                finally:
-                    print("Word " + ip_header[i] + " does not meet the expected format. You must provide a string with "
-                                                   "multiple four HEX-value words separated by a whitespace.")
-                    return None
-            ip_header[i] = int(ip_header[i], 16)
-            print(ip_header[i])
-    else:
-        print(
-            "Please provide the 16-bit words in HEX-format (without preceding 0x). Example for a 16-bit ip_header: abcd")
+    if packet == "":
+        print("Please provide the 16-bit words in HEX-format (without preceding 0x). Example for a 16-bit ip_header: "
+              "abcd")
         print("It is assumed that the IHL is 5.")
 
         ip_header = []
@@ -52,76 +44,138 @@ def ip(packet=""):
         ip_header.append(int(input("Second Part of Source IP-Address: "), 16))
         ip_header.append(int(input("First Part of Destination IP-Address: "), 16))
         ip_header.append(int(input("Second Part of Destination IP-Address: "), 16))
+    else:
+        ip_header = packet.split(" ")
+
+    for i in range(len(ip_header)):  # do some input validation
+        print(ip_header[i])
+        if len(ip_header[i]) != 4:
+            try:
+                hex(int(ip_header[i], 16))
+            except TypeError:
+                print("TypeError occurred.")
+            finally:
+                print("Word " + ip_header[i] + " does not meet the expected format. You must provide a string with "
+                                               "multiple four HEX-value words separated by a whitespace.")
+                return None
+        ip_header[i] = int(ip_header[i], 16)
+        print(ip_header[i])
 
     checksum = calc_checksum(ip_header)
     print("Checksum as Integer (DEC): " + str(checksum))
     return hex(checksum)
 
 
-def tcp(src_ip, dest_ip, protocol="06", tcp_segment=""):
-    # 0                   1                   2                   3
-    # 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-    # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    # |          Source Port          |       Destination Port        |
-    # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    # |                        Sequence Number                        |
-    # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    # |                    Acknowledgment Number                      |
-    # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    # |  Data |           |U|A|P|R|S|F|                               |
-    # | Offset| Reserved  |R|C|S|S|Y|I|            Window             |
-    # |       |           |G|K|H|T|N|N|                               |
-    # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    # |           Checksum            |         Urgent Pointer        |
-    # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    # |                    Options                    |    Padding    |
-    # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    # |                             data                              |
-    # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    #
-    # TCP Header Format
+def tcp(ip_packet):
+    """
+    Calculate the checksum in a tcp-segment
 
-    # create the pseudo header: src ip, dest ip, 1 byte reserved (0-filled), protocol from ip-header, tcp segment length
-    # calculate checksum over pseudo header and the whole tcp segment
-    # the algorithm to calculate the checksum is the same as for ip-header checksum
-    tcp_segment = tcp_segment.split()
-    for i in range(len(tcp_segment)):  # do some input validation
-        try:
-            if len(tcp_segment[i]) != 4:
-                if i == (len(tcp_segment) - 1):
-                    while len(tcp_segment[i]) < 4:
-                        tcp_segment[i] = tcp_segment[i] + '0'
+    Provide the IP-packet in the parameter as hex-values (without leading '0x', in form of 16-bit words separated by a
+    whitespace. The format of the TCP-header is as follows:
+
+    0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |          Source Port          |       Destination Port        |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |                        Sequence Number                        |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |                    Acknowledgment Number                      |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |  Data |           |U|A|P|R|S|F|                               |
+    | Offset| Reserved  |R|C|S|S|Y|I|            Window             |
+    |       |           |G|K|H|T|N|N|                               |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |           Checksum            |         Urgent Pointer        |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |                    Options                    |    Padding    |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    |                             data                              |
+    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    TCP Header Format
+
+    create the pseudo header: src ip, dest ip, 1 byte reserved (0-filled), protocol from ip-header, tcp segment length
+    calculate checksum over pseudo header and the whole tcp segment
+    the algorithm to calculate the checksum is the same as for ip-header checksum
+    Check out RFC 793 for more information about the specification.
+
+
+    :param ip_packet: A complete IP-packet (inclusive payload), with 'next-level protocol' set to 06 (TCP).
+    :return: Checksum of the TCP-segment provided in the payload of the IP-packet
+    """
+
+    print("Started calculating the tcp checksum with ip-packet: " + ip_packet)
+    # Do some input validation.
+    try:
+        if not isinstance(ip_packet, str):
+            raise TypeError
+        ip_packet = ip_packet.split(" ")
+        for i in range(len(ip_packet)):
+            if (len(ip_packet[i]) != 4) and (i != (len(ip_packet) - 1)):
                 raise ValueError
-            hex(int(tcp_segment[i], 16))
-        except TypeError:
-            print("Your package is broken. You must provide HEX-values.")
-            return None
-        except ValueError:
-            print("Please provide the input in form of 16-bit HEX-words separated by a whitespace.")
-            return None
-    segment_length = 2 * len(tcp_segment)  # 16 bit long, shows how long the whole segment in bytes is
+            hex(int(ip_packet[i], 16))
+    except TypeError:
+        print("Your package is broken. You must provide HEX-values.")
+        return None
+    except ValueError:
+        print("Please provide the input in form of 16-bit HEX-words separated by a whitespace.")
+        return None
+
+    # extract some information from the ip header and determine where the tcp-segment starts
+    internet_header_length = ip_packet[0][1]
+    internet_header_length = int(internet_header_length, 16) * 4  # IHL in bytes
+
+    ip_header = ip_packet[:(internet_header_length / 2)]  # ip-packet is a list with 16-bit words, not with bytes
+    tcp_segment = ip_packet[(internet_header_length / 2):]
+
+    protocol = ip_header[9][2:]  # next-level protocol field from the ip-header
+    src_ip = ' '.join([ip_header[12], ip_header[13]])  # source ip-address in hex format
+    dest_ip = ' '.join([ip_header[14], ip_header[15]])  # destination ip-address in hex format
+
+    # calculate the total length of the ip-packet in bytes (later used for determining the tcp-segment length)
+    if len(ip_packet[-1]) == 4:
+        ip_total_length = 2 * len(ip_packet)
+    elif len(ip_packet[-1]) == 2:
+        ip_total_length = 2 * len(ip_packet) - 1
+        tcp_segment[-1] = tcp_segment[-1] + '00'  # for checksum-calculation, add trailing zeros (according to RFC 793)
+    else:
+        # this case does not exist
+        ip_total_length = 0
+        raise Exception
+
+    # Length of the TCP-segment in bytes (field is 16 bit long),  used in the pseudo header, calculation is done
+    # according to the calculation in wireshark
+    segment_length = ip_total_length - internet_header_length
     segment_length = hex(segment_length)[2:]
     while len(segment_length) < 4:
         segment_length = '0' + segment_length
+    print("Segment is 0x" + segment_length + " byte long.")
 
-    pseudo_header = src_ip + " " + dest_ip + " 00" + protocol + " " + segment_length  # 96 bit long (3 x 32 bit)
-    pseudo_complete_segment = pseudo_header + ' ' + ' '.join(tcp_segment)
-    if len(pseudo_complete_segment) % 2 == 1:
-        pseudo_complete_segment += " 0000"
-    print(pseudo_complete_segment)
-    pseudo_complete_segment = pseudo_complete_segment.split(" ")
+    # construct the packet for calculating the checksum
+    pseudo_header = src_ip + " " + dest_ip + " 00" + protocol + " " + segment_length  # according to specs: 96 bit long
+    extended_segment = pseudo_header + ' ' + ' '.join(tcp_segment)
+    print("TCP-Segment inclusive pseudo header: " + extended_segment)
+    extended_segment = extended_segment.split(" ")
 
-    for i in range(len(pseudo_complete_segment)):
-        pseudo_complete_segment[i] = int(pseudo_complete_segment[i], 16)
+    # prepare the extended segment for the actual checksum calculation
+    for i in range(len(extended_segment)):
+        extended_segment[i] = int(extended_segment[i], 16)
 
-    checksum = calc_checksum(pseudo_complete_segment)
-    print(checksum)
-    print(hex(checksum))
+    checksum = calc_checksum(extended_segment)
+    print("Checksum is: " + str(checksum) + " and in HEX-format: " + hex(checksum))
     return hex(checksum)
 
 
 def ones_complement_addition(number1, number2):
-    # to see how the one's complement addition works, visit: https://youtu.be/EmUuFRMJbss
+    """
+    Build the one's complement addition as used in the calculation of IP-, TCP- and UDP-headers.
+
+    To see how the one's complement addition works, visit: https://youtu.be/EmUuFRMJbss
+    :param number1: A 16-bit number as Integer
+    :param number2: A 16-bit number as Integer
+    :return: One's complement of the two numbers
+    """
+
     print("calculating the one's complement of " + str(number1) + " + " + str(number2))
     if not (isinstance(number1, int)) and not (isinstance(number2, int)):
         return None
@@ -134,7 +188,7 @@ def ones_complement_addition(number1, number2):
         result = '0b' + partial_result
 
     if len(result) > 18:
-        if len(result) == 19 and result[2] == '1':  # TODO: Add code to handle the cases where the result is even longer
+        if len(result) == 19 and result[2] == '1':
             print("carry bit needed")
             carry_bit = '1'
             result = list(result)  # convert the string to a list
@@ -157,15 +211,30 @@ def ones_complement_addition(number1, number2):
     return int(result, 2)
 
 
-def calc_checksum(segment):
-    # `segment` needs to be a list with integers
-    # this function is only for use in environments where 16-bit words are used.
-    # TODO: Add validation for the parameter
+def calc_checksum(packet):
+    """
+    Calculates the checksum of the given packet.
+
+    This function is only for use in environments where 16-bit words are used.
+
+    :param packet: A list with 16-bit numbers as Integers.
+    :return:
+    """
+
+    # validate the input
+    try:
+        if not isinstance(packet, list):
+            raise TypeError
+        for i in range(len(packet)):
+            if not isinstance(packet, int):
+                raise TypeError
+    except TypeError:
+        print("Wrong type used for method 'calc_checksum(packet)'")
 
     print("Calculating the checksum...")
-    checksum = ones_complement_addition(segment[0], segment[1])
-    for i in range(2, len(segment)):
-        checksum = ones_complement_addition(checksum, segment[i])
+    checksum = ones_complement_addition(packet[0], packet[1])
+    for i in range(2, len(packet)):
+        checksum = ones_complement_addition(checksum, packet[i])
     checksum = bin(checksum)
 
     if len(checksum) < 18:  # add leading zeros
@@ -221,5 +290,7 @@ def main(protocol):
 
 
 # to calculate the checksum for the packet crafter:
-# print(ip("4500 0028 abcd 0000 4006 0000 c0a8 009e c0a8 002b"))
-print(tcp("c0a8 009e", "c0a8 002b", "06", "3039 0050 0000 0000 0000 0000 5002 7110 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000"))
+# print(ip("4500 003b abcd 0000 4006 0000 ac11 b0b1 ac11 b0b3"))
+
+print(tcp("4500 003c abcd 0000 4006 caf2 c0a8 c151 c0a8 c159 ff98 0050 0000 0000 0000 0000 5002 7110 3ad9 0000 0000 "
+          "0000 0000 0000 0000 0000 0000 0000 0000 0000"))
