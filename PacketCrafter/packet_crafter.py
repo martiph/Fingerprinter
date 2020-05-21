f634f9c1ad9fcedfa09011dd7012151442ec4291
martiph <philip_marti@besonet.ch>
2020-05-21 16:50:09 +0200

    Cleaned up the ip method. Completely refactored the tcp method. Added more comments/docstrings. Improved calculation of TCP-Segment length.

--- PacketCrafter/packet_crafter.py
+++ PacketCrafter/packet_crafter.py
@@ -1,20 +1,48 @@
 import sys
+import re
 import socket
 import calculate_header_checksum as calc_check
 
+
+def convert_ip_address(ip_address):
+    """
+    This function converts a ipv4 address in standard string format to a HEX representation
+
+    :param ip_address: string with IPv4 address in format '192.168.0.1'
+    :return: HEX representation of IPv4 address (string)
+    """
+    if re.search('^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$', ip_address) is None:
+        return None
+    ip_addr = ip_address.split('.')
+    for i in range(4):
+        ip_addr[i] = hex(int(ip_addr))[2:]
+        while len(ip_addr[i]) < 2:
+            ip_addr = '0' + ip_addr
+    ip_address = ip_addr[0] + ip_addr[1] + ' ' + ip_addr[2] + ip_addr[3]
+    return ip_address
+
+
+# Tutorial on how to craft manually a raw ip-packet:
 # https://inc0x0.com/tcp-ip-packets-introduction/tcp-ip-packets-3-manually-create-and-send-raw-tcp-ip-packets/
+
+
+# variables
+src_ip = '192.168.0.158'
+dest_ip = '192.168.0.43'
+
 
 # create a raw IPv4 socket
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW)
 # set the socket option IP_HDRINCL to 1, to tell the kernel that a ip-header is provided
 s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
+
 # create the ip-header
 ip_header = '4500 003c'  # Version, IHL, Type of Service | Total Length (inclusive data, in bytes)
 ip_header += ' abcd 0000'  # Identification | Flags, Fragment Offset
 ip_header += ' 4006 0000'  # TTL, Protocol | Header Checksum
-ip_header += ' ac0b e8a1'  # Source Address 172.17.232.161
-ip_header += ' ac0b e8a1'  # Destination Address 172.17.232.161
+ip_header += ' ' + convert_ip_address(src_ip)  # Source Address 172.17.232.161
+ip_header += ' ' + convert_ip_address(dest_ip)  # Destination Address 172.17.232.161
 
 # create the tcp-header
 tcp_header = 'ff99 ff98'  # Source Port (65433) | Destination Port (65432)
@@ -35,8 +63,10 @@ ip_checksum = calc_check.ip(ip_header)
 ip_header = ip_header.split(' ')
 ip_header[5] = ip_checksum[2:]  # return value of calc_check.ip() is prefixed with '0x'
 
-local_ip = str(int(ip_header[6][:2], 16)) + '.' + str(int(ip_header[6][2:], 16)) + '.' + str(int(ip_header[7][:2], 16)) + '.' + str(int(ip_header[7][2:], 16))
-remote_ip = str(int(ip_header[8][:2], 16)) + '.' + str(int(ip_header[8][2:], 16)) + '.' + str(int(ip_header[9][:2], 16)) + '.' + str(int(ip_header[9][2:], 16))
+local_ip = str(int(ip_header[6][:2], 16)) + '.' + str(int(ip_header[6][2:], 16)) + '.' + str(
+    int(ip_header[7][:2], 16)) + '.' + str(int(ip_header[7][2:], 16))
+remote_ip = str(int(ip_header[8][:2], 16)) + '.' + str(int(ip_header[8][2:], 16)) + '.' + str(
+    int(ip_header[9][:2], 16)) + '.' + str(int(ip_header[9][2:], 16))
 dest_port = int(tcp_header.split(' ')[1], 16)
 
 tcp_checksum = calc_check.tcp(' '.join(ip_header) + ' ' + tcp_header)  # + ' ' + tcp_payload)
